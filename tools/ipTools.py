9b4960d399f35b104b411b655a85b5d030c6af2e
Sloobot <42865323+Sloobot@users.noreply.github.com>
2021-10-23 10:05:56 +0300

    Add files via upload

---
+++ tools/ipTools.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+#MIT License
+#
+#Copyright (c) 2021 Sloobot
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+import socket
+import requests
+import ipaddress
+from urllib.parse import urlparse
+
+# Check if site is in cloudflare
+def isCloudFlare(link):
+	parsed_uri = urlparse(link)
+	domain = '{uri.netloc}'.format(uri = parsed_uri)
+	try:
+		origin = socket.gethostbyname(domain)
+		iprange = requests.get('https://www.cloudflare.com/ips-v4').text
+		ipv4 = [row.rstrip() for row in iprange.splitlines()]
+		for i in range(len(ipv4)):
+			if ipaddress.ip_address(origin) in ipaddress.ip_network(ipv4[i]):
+				return True
+	except socket.gaierror:
+		print("\033[1;31m"+"[-]"+"\033[0m"+"Unable to verify if victim's IP address belong to a CloudFlare\'s subnet!")
+		return False
