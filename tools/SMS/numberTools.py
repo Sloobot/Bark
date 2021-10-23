2201f32e4780f109dcf1b891d2479f3d95970f24
Sloobot <42865323+Sloobot@users.noreply.github.com>
2021-10-23 10:06:47 +0300

    Add files via upload

---
+++ tools/SMS/numberTools.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+#MIT License
+#
+#Copyright (c) 2021 Sloobot
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+from phonenumbers import geocoder, parse
+
+# Make for Russian numbers
+def normalize(phone):
+    if phone[0] == "+":
+        phone = phone[1:]
+    return phone
+
+# Make for other services
+def transformPhone(phone, i):
+	if i == 5:
+		return '+' + phone[0] + ' (' + phone[1:4] + ') ' + phone[4:7] + ' ' + phone[7:9] + ' ' + phone[9:11]
+
+# Get country name by phone
+def getCountry(phone):
+	query = parse("+" + phone, None)
+	return repr(geocoder.description_for_number(query, 'en'))
