8e7dc401073546d4d5d424c7637a3f4ff9da1579
Sloobot <42865323+Sloobot@users.noreply.github.com>
2021-10-23 10:05:41 +0300

    Create randomData.py

---
+++ tools/randomData.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+#MIT License
+#
+#Copyright (c) 2021 Sloobot
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+import json
+import random
+
+# Get random IP
+def random_IP():
+    ip = []
+    for _ in range(0, 4):
+        ip.append(str(random.randint(1,255)))
+    return ".".join(ip)
+
+# Get random referer
+def random_referer():
+    with open("tools/L7/referers.txt", 'r') as referers:
+    	referers = referers.readlines()
+    return random.choice(referers)
+
+# Get random user agent
+def random_useragent():
+    with open("tools/L7/user_agents.json", 'r') as agents:
+        user_agents = json.load(agents)["agents"]
+    return random.choice(user_agents)
